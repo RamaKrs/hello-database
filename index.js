af10f45914285f3a36f21b9b88bf83dc5ce9d275
RamaKrs <58743102+RamaKrs@users.noreply.github.com>
2020-08-21 16:22:40 -0300

    Mongo, y database creadas

--- index.js
+++ index.js
@@ -4,6 +4,9 @@
 const express  = require('express');
 const mongoose = require('mongoose');
 
+// importamos el router que creamos para la api
+const router = require('./routes/api/user');
+
 // puerto y base de datos
 const port = process.env.PORT        || 3000;
 const db   = process.env.MONGODB_URI || 'mongodb://localhost/hellodb';
@@ -19,6 +22,9 @@ mongoose
     console.log(`DB connected @ ${db}`);
   })
 .catch(err => console.error(`Connection error ${err}`));
+
+// usamos el router
+app.use('/api', router);
 
 // el server escucha todo
 app.listen(port, () => {
